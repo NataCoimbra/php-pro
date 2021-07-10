02b0f287639cc9d97b88c609b0ae4382a73808d3
NataCoimbra <nsilvac1930@gmail.com>
2021-07-10 15:34:29 -0300

    Primeiro commit

---
+++ app/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace app\controllers;
+
+class Home {
+
+    public function index(){
+        var_dump('Home');
+        die();
+    }
+
+    public function create(){
+        var_dump('create');
+        die();
+    }
+}
